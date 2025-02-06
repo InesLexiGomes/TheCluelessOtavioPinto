f772a2393ffc6cddc1e9f995252eaa4d83591761
Inês Gomes <inesgomes.pessoal05@gmail.com>
2025-02-06 21:50:10 +0000

    Removed player bounce to be added as an animation later

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,8 +4,6 @@ public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] private Rigidbody2D rb;
     [SerializeField] private int moveSpeed;
-    [SerializeField] private SpriteRenderer playerSprite;
-    [SerializeField] private int bounceFactor;
     private Vector2 moveDirection;
     private bool active = true;
 
@@ -20,7 +18,6 @@ public class PlayerMovement : MonoBehaviour
     private void Update()
     {
         GetInputs();
-        MovementVisuals();
     }
 
     private void DoMovement()
@@ -34,24 +31,6 @@ public class PlayerMovement : MonoBehaviour
     private void FixedUpdate()
     {
         DoMovement();
-    }
-
-    private void MovementVisuals()
-    {
-        Vector2 spriteBounce = Vector2.zero;
-        if (moveDirection.x != 0)
-        {
-            spriteBounce.x = Mathf.Sin(Time.time)*bounceFactor;
-        }
-        if (moveDirection.y != 0)
-        {
-            spriteBounce.y = Mathf.Sin(Time.time) * bounceFactor;
-        }
-        if (moveDirection.x == 0 && moveDirection.y == 0)
-        {
-            spriteBounce = Vector2.zero;
-        }
-        playerSprite.gameObject.transform.localPosition = spriteBounce;
     }
 
     public void StopMovement()
